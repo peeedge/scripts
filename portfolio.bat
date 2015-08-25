aa6bf66beb66041117197a81214fc17329430962
unknown <543232@BAHR9HWPGG.resource.ds.bah.com>
2015-08-25 13:55:19 -0400

    adding stock

---
+++ portfolio.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\batches
+start chrome.lnk "https://www.google.com/finance?q=NYSE%3AIHS&ei=rancVfjPApe6e__VoYgG"
+start chrome.lnk "https://www.google.com/finance?q=NYSE%3ABAH&ei=rancVfjPApe6e__VoYgG"
+start chrome.lnk "https://www.google.com/finance?q=NASDAQ%3ANFLX&ei=rancVfjPApe6e__VoYgG"
+exit
