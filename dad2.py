213417657f9ad1437b3e2826a752e1771540a52e
Harms <543232@bah.com>
2016-06-17 22:41:02 -0400

    saving the latest

--- dad2.py
+++ dad2.py
@@ -1,12 +1,50 @@
-import numpy as np
+# import numpy as np
 import csv as csv
-import pandas as pd
+# import pandas as pd
 from collections import Counter
 import pprint
+import sys
+
+# class WriteToOutput(object):
+#     def __init__(self, filename="Default.log"):
+#         self.terminal = sys.stdout
+#         self.log = open(filename, "a")
+
+#     def write(self, message):
+#         self.terminal.write(message)
+#         self.log.write(message)
+
+# sys.stdout = WriteToOutput("output.html")
 
 def most_common(lst):
 	# print list.count(lst)
 	return max(set(lst), key=lst.count)
+
+def from_frequency(data):
+	Header = data[0]
+	data.pop(0)
+
+	Header.append("Difference")
+
+	fromList = []
+	output = ""
+	
+	for i in range(len(data)):
+	  fromList.append(data[i][2])
+
+	dictionary = Counter(fromList)
+	sortedDictionary = sorted(dictionary.items(), key=lambda i: i[1], reverse=True)
+
+	output += "<table><thead><th>From</th><th>How Many Times</th></thead><tbody>"
+
+	for key, value in sortedDictionary:
+		output += '<tr><td>%s</td>' % key
+		output += '<td>%d</td></tr>' % value
+
+	output += "</tbody></table>"
+
+	with open("output.html", "w") as my_file:
+		my_file.write(output)
 
 def main():
 	readdata = csv.reader(open("outlookExport.CSV"))
@@ -16,57 +54,7 @@ def main():
 	for row in readdata:
 	  data.append(row)
 
-	Header = data[0]
-	data.pop(0)
-
-	Header.append("Difference")
-
-	#for i in range(len(data)):
-	 # diff = int(data[i][2]) - int(data[i][1])
-	  #data[i].append(diff)
-
-
-	# subject = []
-	fromList = []
-	bodyList = []
-	htmlText = ""
-	bodyWordsList = []
-	for i in range(len(data)):
-	  # subject.append(data[i][0])
-	  fromList.append(data[i][2])
-	  # bodyList.append(data[i][1])
-
-	  # bodyList.append(data[i][1])
-	  # bodyWordsList = data[i][1].split()
-	  # bodyList.extend(bodyWordsList)
-	#print "Total in 1910: %d" % len(pop1910)
-
-	# print Counter(fromList)
-	# print pprint.pformat(Counter(fromList))
-
-	for key, value in Counter(fromList).items():
-		htmlText += ' %s emailed you ' % key
-		htmlText += '  %d times \n' % value
-	# htmlLines = []
-	# for textLine in pprint.pformat(Counter(fromList)).splitlines():
-	#     htmlLines.append('<br/>%s' % textLine) # or something even nicer
-	# htmlText = '\n'.join(htmlLines)
-
-	print htmlText
-
-	# print Counter(fromList).keys()
-	# print Counter(fromList).values()
-
-	# # print most_common(bodyList)
-	# print most_common(fromList)
-	# print set(fromList)
-	# print most_common(subject)
+	from_frequency(data)
 
 
 main()
-
-#print "Average in 1910: %d" % (np.mean(pop1910))
-#print "Standard Deviation in 1910: %d" % (np.std(pop1910)) 
-
-
-#print pd.DataFrame(data, columns = Header)
